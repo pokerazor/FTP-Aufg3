3aeee4e4b837a158ee371df66277d2ec1f5434ff
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2015-06-17 13:21:44 +0200

    some more housekeeping

--- FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Main.java
+++ FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Main.java
@@ -7,17 +7,15 @@ import static SoFTlib.Helper.number;
 import static SoFTlib.Helper.words;
 import SoFTlib.SoFT;
 
-/**
- * @author Marc Gesthüsen, Hanno - Felix Wagner
- *
- */
+/** @author Marc Gesthüsen, Hanno - Felix Wagner */
 public class Main extends SoFT {
-	
-	public static Integer m=0; //Anzahl Knoten gesamt, von Eingabezeile gelesen
-	public Integer F=0; //Anzahl fehlerhafte Knoten, von Eingabezeile gelesen
-	public String[] initialWords = new String[m]; //Wörter als Anfangswerte der Knoten, von Eingabezeile gelesen
-	public Integer d=0; //Dauer der Protokoll-Phase
-	
+
+	public static Integer m = 0; // Anzahl Knoten gesamt, von Eingabezeile gelesen
+	public Integer F = 0; // Anzahl fehlerhafte Knoten, von Eingabezeile gelesen
+	public String[] initialWords = new String[m]; // Wörter als Anfangswerte der Knoten, von Eingabezeile gelesen
+	public Integer d = 0; // Dauer der Protokoll-Phase
+	public String inputLine="";
+
 	/**
 	 * 
 	 */
@@ -25,31 +23,30 @@ public class Main extends SoFT {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param args
-	 */
+	/** @param args */
 	public static void main(String[] args) {
 		Node[] nodes = { new Node(m) };
 		new Main().runSystem(nodes, "Signed Messages Protokoll", "Aufgabe 3", "Marc Gesthüsen, Hanno - Felix Wagner");
 	}
-	
+
 	public int result(String input, String[] output) {
 		Integer resultVal = -1;
-		if (false){
+		if (false) {
 			resultVal = 0; // wenn IC1 und IC2 erfüllt sind (d.h. Übereinstimmung erreicht worden ist)
-		} else if(false){
+		} else if (false) {
 			resultVal = 1; // wenn nur IC1 erfüllt ist,
-		} else if(false){
+		} else if (false) {
 			resultVal = 2; // wenn nur IC1 erfüllt ist,
-		} else if(false){
+		} else if (false) {
 			resultVal = 3; // wenn nur IC1 erfüllt ist,
 		} else {
 			resultVal = 4;
 		}
 		return resultVal;
 	}
-	
-	private static void parseInputLine(){
-		
+
+	private void parseInputLine() {
+		String input="";
+		number(words(input, 1, 1, 1));
 	}
 }
--- FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Node.java
+++ FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Node.java
@@ -5,33 +5,48 @@ package de.unidue.stud.maha.ftpa3;
 
 import SoFTlib.SoFTException;
 
-
-/**
- * @author Hanno - Felix Wagner
- *
- */
+/** @author Hanno - Felix Wagner */
 public class Node extends SoFTlib.Node {
-	private Integer m=0; //Anzahl Knoten gesamt, von Eingabezeile gelesen
-	private String[] k = new String[m]; //Konsistenzvektor
-	private Integer majorityShare=0; //m/2
-	private static final String SIGN_FAULTY="-"; //Zeichen für fehlerhaften Knoten
-	private static final String SIGN_TIE="?"; //Zeichen für keine Mehrheit vorhanden/Unentschieden
-	
+	private Integer m = 0; // Anzahl Knoten gesamt, von Eingabezeile gelesen
+	private String[] k = new String[m]; // Konsistenzvektor
+	private Integer majorityShare = 0; // m/2
+	private static final String CHAR_FAULTY = "-"; // Zeichen für fehlerhaften Knoten
+	private static final String CHAR_TIE = "?"; // Zeichen für keine Mehrheit vorhanden/Unentschieden
+	private static final String CHAR_GLUE = " "; // Zeichen für keine Mehrheit vorhanden/Unentschieden
+	private String finalValue="";
+
 	/**
 	 * 
 	 */
 	public Node(Integer m) {
-		this.m=m;
-		k=new String[m];
+		this.m = m;
+		k = new String[m];
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public String runNode(String input) throws SoFTException {
-		return input;
+		String output="";
+		output=buildOutput();
+		return output;
 	}
 
-	private String Mehrheit(String[] k){
+	private String Mehrheit(String[] k) {
 		return k[0];
 	}
-	
+
+	private String buildOutput() { // https://stackoverflow.com/questions/1515437/java-function-for-arrays-like-phps-join
+		String[] s=k;
+		int k = s.length;
+		if (k == 0) {
+			return null;
+		}
+		StringBuilder out = new StringBuilder();
+		out.append(s[0]);
+		for (int x = 1; x < k; ++x) {
+			out.append(CHAR_GLUE).append(s[x]);
+		}
+		out.append(CHAR_GLUE).append(finalValue);
+
+		return out.toString();
+	}
 }
