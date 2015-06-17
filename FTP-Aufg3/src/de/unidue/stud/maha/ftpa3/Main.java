7e12c1596ad839af43e3d8f12ebcf24b7db45ce1
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2015-06-17 16:40:19 +0200

    Nearly finished housekeeping

--- FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Main.java
+++ FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Main.java
@@ -3,34 +3,43 @@
  */
 package de.unidue.stud.maha.ftpa3;
 
-import static SoFTlib.Helper.number;
-import static SoFTlib.Helper.words;
+import static SoFTlib.Helper.*;
 import SoFTlib.SoFT;
 
 /** @author Marc Gesthüsen, Hanno - Felix Wagner */
 public class Main extends SoFT {
 
-	public static Integer m = 0; // Anzahl Knoten gesamt, von Eingabezeile gelesen
+	public Integer m = 0; // Anzahl Knoten gesamt, von Eingabezeile gelesen
+	public Integer majorityShare = 0; // m/2
 	public Integer F = 0; // Anzahl fehlerhafte Knoten, von Eingabezeile gelesen
 	public String[] initialWords = new String[m]; // Wörter als Anfangswerte der Knoten, von Eingabezeile gelesen
 	public Integer d = 0; // Dauer der Protokoll-Phase
-	public String inputLine="";
+	public String inputLine = "";
+	public static Boolean initialized = false;
+	public static final Integer NUMBER_OF_RUNS = 1000;
+	static Node[] nodes = new Node[10];
+	private static final Integer NUMBER_OF_NOTIFICATIONS = 20;
 
-	/**
-	 * 
-	 */
 	public Main() {
-		// TODO Auto-generated constructor stub
 	}
 
 	/** @param args */
 	public static void main(String[] args) {
-		Node[] nodes = { new Node(m) };
-		new Main().runSystem(nodes, "Signed Messages Protokoll", "Aufgabe 3", "Marc Gesthüsen, Hanno - Felix Wagner");
+// java.awt.Toolkit.getDefaultToolkit().beep();
+
+		Main expriment = new Main();
+		for (int i = 0; i < nodes.length; i++) {
+			nodes[i] = new Node(expriment); // create maximum number of nodes, because we can't read the input line before running the experiment. WHY IS THIS SUCH A pita?!
+		}
+
+		expriment.runSystem(nodes, "Signed Messages Protokoll", "Aufgabe 3", "Marc Gesthüsen, Hanno - Felix Wagner");
 	}
 
 	public int result(String input, String[] output) {
 		Integer resultVal = -1;
+
+		this.getSummary();
+		
 		if (false) {
 			resultVal = 0; // wenn IC1 und IC2 erfüllt sind (d.h. Übereinstimmung erreicht worden ist)
 		} else if (false) {
@@ -40,13 +49,47 @@ public class Main extends SoFT {
 		} else if (false) {
 			resultVal = 3; // wenn nur IC1 erfüllt ist,
 		} else {
-			resultVal = 4;
+			resultVal = 4; // else
+			System.out.println(this.getSummary());
+
+		}
+
+		if (exec() > NUMBER_OF_RUNS) {
+			resultVal = 5; // NUMBER_OF_RUNS reached, terminating
+			notifyOfEnd();
 		}
 		return resultVal;
 	}
 
-	private void parseInputLine() {
-		String input="";
-		number(words(input, 1, 1, 1));
+	public synchronized void parseInputLine(String input) {
+		if (!initialized) {
+			inputLine = input;
+
+			m = number(word(inputLine, 1));
+			F = number(word(inputLine, 2));
+			majorityShare=(m/2);
+			initialWords = new String[m];
+			for (int i = 0; i < initialWords.length; i++) {
+				initialWords[i] = word(inputLine, 2 + i); // create maximum number of nodes, because we can't read the input line before running the experiment. WHY IS THIS SUCH A pita?!
+			}
+			initialized = true;
+		}
+	}
+
+	public void notifyOfEnd() {
+		new NotifyOfEnd().start();
+	}
+
+	private class NotifyOfEnd extends Thread {
+		public void run() {
+			for (Integer i = 0; i < NUMBER_OF_NOTIFICATIONS; i++) {
+				java.awt.Toolkit.getDefaultToolkit().beep();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
--- FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Node.java
+++ FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Node.java
@@ -3,39 +3,76 @@
  */
 package de.unidue.stud.maha.ftpa3;
 
+import java.util.HashMap;
+
 import SoFTlib.SoFTException;
 
 /** @author Hanno - Felix Wagner */
 public class Node extends SoFTlib.Node {
-	private Integer m = 0; // Anzahl Knoten gesamt, von Eingabezeile gelesen
-	private String[] k = new String[m]; // Konsistenzvektor
-	private Integer majorityShare = 0; // m/2
+	private Main experiment = null;
+	private String[] k = null; // Konsistenzvektor
 	private static final String CHAR_FAULTY = "-"; // Zeichen für fehlerhaften Knoten
 	private static final String CHAR_TIE = "?"; // Zeichen für keine Mehrheit vorhanden/Unentschieden
 	private static final String CHAR_GLUE = " "; // Zeichen für keine Mehrheit vorhanden/Unentschieden
-	private String finalValue="";
+	private static final String MESSAGE_SUPERFLUOUS = "I am superfluous, terminating.";
+	private String initialWord = "";
+	private String finalValue = CHAR_TIE;
 
 	/**
 	 * 
 	 */
-	public Node(Integer m) {
-		this.m = m;
-		k = new String[m];
-		// TODO Auto-generated constructor stub
+	public Node(Main owner) {
+		this.experiment = owner;
 	}
 
 	public String runNode(String input) throws SoFTException {
-		String output="";
-		output=buildOutput();
+		String output = "";
+		experiment.parseInputLine(input);
+		if (myIndex() > experiment.m - 1) { // terminate nodes which are not necessary
+			say(MESSAGE_SUPERFLUOUS);
+			return MESSAGE_SUPERFLUOUS;
+		}
+
+		k = new String[experiment.m];
+		for (int x = 0; x < k.length; x++) { //initialize consistency vector
+			k[x] = CHAR_TIE;
+		}
+		initialWord = experiment.initialWords[myIndex()];
+		k[myIndex()] = initialWord;
+
+		finalValue=Mehrheit(k);
+		
+		output = buildOutput();
 		return output;
 	}
 
 	private String Mehrheit(String[] k) {
-		return k[0];
+		Integer maxMatches=0;
+		String maxMatchesValue=CHAR_TIE;
+		HashMap<String, Integer> occurences = new HashMap<String, Integer>();
+		for (int i = 0; i < k.length; i++) {
+			String curElement = k[i];
+			if(curElement.equals(CHAR_FAULTY)){
+				continue; // skip entries of faulty nodes
+			}
+			Integer curCount=occurences.get(curElement);
+			if(curCount==null){ // value has no count yet, start from 0
+				curCount=0;
+			}
+			curCount++;
+			occurences.put(curElement, curCount);
+			maxMatches=Math.max(maxMatches, curCount);
+
+			if (maxMatches>experiment.majorityShare){
+				maxMatchesValue=curElement;
+				break; //already found the majority, don't continue
+			}
+		}
+		return maxMatchesValue;
 	}
 
 	private String buildOutput() { // https://stackoverflow.com/questions/1515437/java-function-for-arrays-like-phps-join
-		String[] s=k;
+		String[] s = k;
 		int k = s.length;
 		if (k == 0) {
 			return null;
