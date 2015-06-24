53145f9ee4632a3eb6b78b0dfa7e68f17d1b306c
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2015-06-24 14:08:49 +0200

    add some more housekeeping in runup for implementation of result and nodes

--- FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Main.java
+++ FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Main.java
@@ -9,6 +9,8 @@ import SoFTlib.SoFT;
 /** @author Marc Gesthüsen, Hanno - Felix Wagner */
 public class Main extends SoFT {
 
+	// TEST INPUT LINE 4 1 rot orange gruen orange
+
 	public Integer m = 0; // Anzahl Knoten gesamt, von Eingabezeile gelesen
 	public Integer majorityShare = 0; // m/2
 	public Integer F = 0; // Anzahl fehlerhafte Knoten, von Eingabezeile gelesen
@@ -25,8 +27,6 @@ public class Main extends SoFT {
 
 	/** @param args */
 	public static void main(String[] args) {
-// java.awt.Toolkit.getDefaultToolkit().beep();
-
 		Main expriment = new Main();
 		for (int i = 0; i < nodes.length; i++) {
 			nodes[i] = new Node(expriment); // create maximum number of nodes, because we can't read the input line before running the experiment. WHY IS THIS SUCH A pita?!
@@ -35,22 +35,36 @@ public class Main extends SoFT {
 		expriment.runSystem(nodes, "Signed Messages Protokoll", "Aufgabe 3", "Marc Gesthüsen, Hanno - Felix Wagner");
 	}
 
+	// IC1: Alle fehlerfreien Knoten terminieren mit dem gleichen Konsistenzvektor
+	// IC2: Wenn der Erzeuger fehlerfrei ist, dann wird sein Anfangswert von jedem fehlerfreien Knoten in den Konsistenzvektor übernommen
 	public int result(String input, String[] output) {
-		initialized=false;
+		initialized = false;
 		Integer resultVal = -1;
 
-		this.getSummary();
+		String summary = " ";
+
+		for (int i = 0; i < output.length; i++) {
+			String curOutput = output[i];
+			summary += i + ":\"" + curOutput + "\" ";
+
+		}
+
+		setSummary(fault(0) + " m =" + m + " F =" + F + " majorityShare =" + majorityShare + "\n" + summary);
+
+		String[] foo = explodeWords(output[0]);
+
+		setSummary(foo[0] + "");
 
 		if (false) {
 			resultVal = 0; // wenn IC1 und IC2 erfüllt sind (d.h. Übereinstimmung erreicht worden ist)
 		} else if (false) {
 			resultVal = 1; // wenn nur IC1 erfüllt ist,
 		} else if (false) {
-			resultVal = 2; // wenn nur IC1 erfüllt ist,
+			resultVal = 2; // wenn nur IC2 erfüllt ist,
 		} else if (false) {
-			resultVal = 3; // wenn nur IC1 erfüllt ist,
+			resultVal = 3; // wenn IC1 und IC2 verletzt sind
 		} else {
-			resultVal = 4; // else
+			resultVal = 4; // else, in komischen fällen
 		}
 
 		if (exec() > NUMBER_OF_RUNS || output[0].equals(Node.MESSAGE_INVALID)) {
@@ -60,6 +74,35 @@ public class Main extends SoFT {
 		return resultVal;
 	}
 
+	public Integer[] getFaultVector() {
+		Integer[] faultVector = new Integer[m];
+		for (int i = 0; i < m; i++) {
+			faultVector[i] = fault(i);
+		}
+		return faultVector;
+	}
+
+	public Boolean[] getIntegrityVector() {
+		Integer[] faultVector = getFaultVector();
+
+		Boolean[] integrityVector = new Boolean[faultVector.length];
+		for (int i = 0; i < m; i++) {
+			integrityVector[i] = faultVector[i]==0; //if node is faultless, return true integrity
+		}
+		return integrityVector;
+	}
+	
+	public String getIntegrityGroupString(){
+		Integer[] faultVector = getFaultVector();
+		String integrityGroupString="";
+		for (int i = 0; i < m; i++) {
+			if(faultVector[i]==0){
+				or(integrityGroupString,nodeChr (i)+""); //add node char to list of faultless noodes
+			}
+		}
+		return integrityGroupString;
+	}
+
 	public synchronized Boolean parseInputLine(String input) {
 		if (!initialized) {
 			inputLine = input;
@@ -67,20 +110,36 @@ public class Main extends SoFT {
 
 			m = number(word(inputLine, 1));
 			F = number(word(inputLine, 2));
-			
-			if(wordCount<4 || m<2 || wordCount-2!=m || F>m){ // no sense in less than 2 nodes, check if number of inputs matches number of words, not too many faulty nodes
+
+			if (wordCount < 4 || m < 2 || wordCount - 2 != m || F > m) { // no sense in less than 2 nodes, check if number of inputs matches number of words, not too many faulty nodes
 				return false;
 			}
-			
+
 			majorityShare = (m / 2);
+			initialWords = explodeWords(inputLine, 3);
+			/*
 			initialWords = new String[m];
 			for (int i = 0; i < initialWords.length; i++) {
-				initialWords[i] = word(inputLine, 3 + i); // create maximum number of nodes, because we can't read the input line before running the experiment. WHY IS THIS SUCH A pita?!
+				initialWords[i] = word(inputLine, 3 + i); 
 			}
+			*/
 
 			initialized = true;
 		}
 		return initialized;
+	}
+
+	public static String[] explodeWords(String inputLine, Integer offset) {
+		Integer numOfElements = getWordCount(inputLine, 1);
+		String[] elements = new String[numOfElements];
+		for (int i = 0; i < elements.length; i++) {
+			elements[i] = word(inputLine, offset + i);
+		}
+		return elements;
+	}
+
+	public static String[] explodeWords(String inputLine) {
+		return explodeWords(inputLine, 1);
 	}
 
 	public void notifyOfEnd() {
--- FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Node.java
+++ FTP-Aufg3/src/de/unidue/stud/maha/ftpa3/Node.java
@@ -10,14 +10,17 @@ import SoFTlib.SoFTException;
 /** @author Hanno - Felix Wagner */
 public class Node extends SoFTlib.Node {
 	private Main experiment = null;
-	private String[] k = null; // Konsistenzvektor
 	private static final String CHAR_FAULTY = "-"; // Zeichen für fehlerhaften Knoten
 	private static final String CHAR_TIE = "?"; // Zeichen für keine Mehrheit vorhanden/Unentschieden
 	private static final String CHAR_GLUE = " "; // Zeichen für keine Mehrheit vorhanden/Unentschieden
 	public static final String MESSAGE_SUPERFLUOUS = "I am superfluous, terminating.";
 	public static final String MESSAGE_INVALID = "Input line invalid, terminating.";
-	private String initialWord = "";
+	private String[] k = null; // Konsistenzvektor, k1...km
+
+	private String initialWord = ""; // wj
 	private String finalValue = CHAR_TIE;
+	private Integer currentPhase = 0; // i
+	private Integer senderIndex = 0; // von Rechner Re wird eine Nachricht empfangen
 
 	/**
 	 * 
@@ -28,7 +31,7 @@ public class Node extends SoFTlib.Node {
 
 	public String runNode(String input) throws SoFTException {
 		String output = "";
-		if(!experiment.parseInputLine(input)){
+		if (!experiment.parseInputLine(input)) {
 			say(MESSAGE_INVALID);
 			return MESSAGE_INVALID;
 		}
@@ -38,39 +41,39 @@ public class Node extends SoFTlib.Node {
 		}
 
 		k = new String[experiment.m];
-		for (int x = 0; x < k.length; x++) { //initialize consistency vector
+		for (int x = 0; x < k.length; x++) { // initialize consistency vector
 			k[x] = CHAR_TIE;
 		}
 		initialWord = experiment.initialWords[myIndex()];
 
 		k[myIndex()] = initialWord;
 
-		finalValue=Mehrheit(k);
-		
+		finalValue = Mehrheit(k);
+
 		output = buildOutput();
 		return output;
 	}
 
 	private String Mehrheit(String[] k) {
-		Integer maxMatches=0;
-		String maxMatchesValue=CHAR_TIE;
+		Integer maxMatches = 0;
+		String maxMatchesValue = CHAR_TIE;
 		HashMap<String, Integer> occurences = new HashMap<String, Integer>();
 		for (int i = 0; i < k.length; i++) {
 			String curElement = k[i];
-			if(curElement.equals(CHAR_FAULTY)){
+			if (curElement.equals(CHAR_FAULTY)) {
 				continue; // skip entries of faulty nodes
 			}
-			Integer curCount=occurences.get(curElement);
-			if(curCount==null){ // value has no count yet, start from 0
-				curCount=0;
+			Integer curCount = occurences.get(curElement);
+			if (curCount == null) { // value has no count yet, start from 0
+				curCount = 0;
 			}
 			curCount++;
 			occurences.put(curElement, curCount);
-			maxMatches=Math.max(maxMatches, curCount);
+			maxMatches = Math.max(maxMatches, curCount);
 
-			if (maxMatches>experiment.majorityShare){
-				maxMatchesValue=curElement;
-				break; //already found the majority, don't continue
+			if (maxMatches > experiment.majorityShare) {
+				maxMatchesValue = curElement;
+				break; // already found the majority, don't continue
 			}
 		}
 		return maxMatchesValue;
